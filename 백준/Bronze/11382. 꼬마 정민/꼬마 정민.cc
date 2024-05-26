22b1f8d360483a8970c508b0ff0e0d71f418e598
jaydenys5959 <89845731+jaydenys5959@users.noreply.github.com>
2024-05-26 20:13:05 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void) {
+  long long val1;
+  std::cin >> val1;
+  long long val2;
+  std::cin >> val2;
+  long long val3;
+  std::cin >> val3; 
+  long long result = val1 + val2 + val3;
+  std::cout << result;
+}
