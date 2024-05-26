deaf5651a58c2f47c50da12f9c4ff59ef931869a
jaydenys5959 <89845731+jaydenys5959@users.noreply.github.com>
2024-05-26 20:09:24 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+int main(void) {
+  int val1;
+  std::cin >> val1;
+  int val2;
+  std::cin >> val2;    
+  int result = val1 + val2;
+  std::cout<< result <<"\n";
+  result = val1 - val2;
+  std::cout<< result <<"\n";
+  result = val1 * val2;
+  std::cout<< result <<"\n";
+  result = val1 / val2;
+  std::cout<< result << "\n";
+  result = val1 % val2;
+  std::cout<< result << "\n";
+}
