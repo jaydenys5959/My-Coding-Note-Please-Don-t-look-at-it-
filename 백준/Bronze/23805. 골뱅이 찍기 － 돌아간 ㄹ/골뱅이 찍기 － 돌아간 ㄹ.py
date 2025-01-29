ceb7bb1cd7ab61993eb2c8fd0fef70617485234d
jaydenys5959 <89845731+jaydenys5959@users.noreply.github.com>
2025-01-29 16:43:17 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.py
@@ -0,0 +1,11 @@
+N = int(input())
+for z in range(N) : 
+    print("@@@" * N + " " * N + "@" * N)
+for z in range(N) : 
+    print("@"*N + " " * N +"@"*N+" " * N +"@"*N)
+for z in range(N) : 
+    print("@"*N + " " * N +"@"*N+" " * N+"@"*N)
+for z in range(N) : 
+    print("@"*N + " " * N +"@"*N+" " * N +"@"*N)
+for z in range(N) : 
+    print("@" * N +  " " * N  + "@@@" * N)
