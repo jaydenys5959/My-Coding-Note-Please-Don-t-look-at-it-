9cbdfe5728430a25cfc041c5f5eeff5c84793f69
jaydenys5959 <89845731+jaydenys5959@users.noreply.github.com>
2025-03-01 17:57:26 +0900

    [Silver V] Title: 영화감독 숌, Time: 660 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+n = int(input())
+number = 666
+count = 0
+while True:
+    if "666" in str(number):
+        count += 1
+    if n == count:
+        print(number)
+        break
+    number += 1    
+        
+        
