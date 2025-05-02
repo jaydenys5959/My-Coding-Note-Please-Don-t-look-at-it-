e2d9c5da307511abd9035bb28311b490c8fa3d43
jaydenys5959 <89845731+jaydenys5959@users.noreply.github.com>
2025-05-02 15:38:51 +0900

    [Bronze IV] Title: 빵, Time: 48 ms, Memory: 31388 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,15 +1,11 @@
 N = int(input())
-ans = 1001
-for i in range(N):
-    A,B = map(int,input().split())
-
-    if A <= B: # 내가 빵을  살 수 있는 경우라면,
-        if B < ans:
-            ans = B         
-if 1001 == ans:
+min = 1001
+for n in range(N):
+    time,bread = map(int,input().split())
+    if time <= bread:
+        if bread < min:
+            min = bread
+if min == 1001:
     print(-1)
 else:
-    print(ans)
-        
-    
- 
+    print(min)            
