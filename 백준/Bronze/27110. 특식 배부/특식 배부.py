acb196198023d4868d1b394dc5b43c8226180ef2
jaydenys5959 <89845731+jaydenys5959@users.noreply.github.com>
2025-05-16 20:34:33 +0900

    [Bronze IV] Title: 특식 배부, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/27110. 특식 배부/특식 배부.py
+++ 백준/Bronze/27110. 특식 배부/특식 배부.py
@@ -1,6 +1,10 @@
 N = int(input())
 A,B,C = map(int,input().split()) # A는 후라이드 B는 간장 C는 양념❤
-
-ans = min(A,N) + min(B,N) + min(C,N)
-
+ans = 0
+D = [A,B,C]
+for i in D:
+	if i > N:
+		ans += N
+	elif i <= N:
+		ans += i
 print(ans)
